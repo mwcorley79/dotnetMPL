adf231a6da08d8132ea31d53f913b7d5a4498d8f
Mike Corley <mwcorley79@gmail.com>
2020-09-10 11:57:02 -0400

    update messsage test

--- MessageTest/Program.cs
+++ MessageTest/Program.cs
@@ -1,20 +1,73 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using MPL;
 
-namespace MessageTest
-{
-    class Program
+namespace MPL_Tests
+{  
+    public class Program
     {
+        public static bool test()
+        {
+            try
+            {
+                Console.WriteLine("Testing Message class");
+
+                Console.Write("Testing string parameterized constuctor...");
+                Message msg1 = new Message("This is a simple message from Mike", MessageType.BINARY);
+                if (msg1.ToString() != "This is a simple message from Mike")
+                {
+                    Console.WriteLine("Failed");
+                    return false;
+                }
+                else
+                    Console.WriteLine("Passed!");
+
+                Console.Write("Testing byte[] parameterized constuctor...");
+                Message msg2 = new Message("This is a simple message from Mike", MessageType.DEFAULT);
+                byte[] b = Encoding.ASCII.GetBytes("This is a simple message from Mike");
+
+                if (b.Length != msg2.Length)
+                {
+                    Console.WriteLine("Failed");
+                    return false;
+                }
+                
+                for(int i = 0; i < b.Length; ++i)    
+                {
+                    if (b[i] != msg2.GetData[i])
+                    {
+                        Console.WriteLine("Failed");
+                        return false;
+                    }
+                }
+               
+                Console.WriteLine("Passed!");
+
+
+                Console.Write("Testing Type Accessor function...");
+                if (msg1.Type != MessageType.BINARY || msg2.Type != MessageType.DEFAULT)
+                {
+                    Console.WriteLine("Failed");
+                    return false;
+                }
+                else
+                    Console.WriteLine("Passed!");
+
+                Console.WriteLine("All tests passed!");
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Message Class");
-
-            Console.WriteLine("Test parameterized Message constructor for string");
-            Message msg1 = new Message("This is a simple message from Mike", MessageType.DEFAULT);
-            Console.WriteLine("The string is: {0}, Message type is: {1}", msg1, msg1.Type);
-
-            Message StopMsg = new Message(MessageType.STOP_SENDING);
-
+            Program.test();
         }
     }
 }
